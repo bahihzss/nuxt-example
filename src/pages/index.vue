66b1e411053848654eb709599c3e01e5014ef546
kawamoto <bahihzss@gmail.com>
2021-08-30 00:07:16 +0900

    UIを作成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,55 @@
 <template>
   <div class="PageIndex">
-    Hello, Nuxt.
+    <form @submit.prevent>
+      <input
+        type="text"
+        v-model="newTaskTitle"
+      >
+      <button
+        @click="handleAddButtonClick()"
+        :disabled="!newTaskTitle.length"
+      >
+        追加
+      </button>
+    </form>
+    <ul>
+      <li
+        :key="task.id"
+        v-for="task in tasks"
+      >
+        {{ task.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import { useTasks } from '~/compositions/tasks'
 
 export default defineComponent({
   name: 'PageIndex',
 
   components: {},
 
-  setup () {},
+  setup () {
+    const { tasks, create, done } = useTasks()
+
+    const newTaskTitle = ref('')
+
+    const handleAddButtonClick = async () => {
+      if (newTaskTitle.value) {
+        await create(newTaskTitle.value)
+      }
+    }
+
+    return {
+      tasks,
+      done,
+      newTaskTitle,
+      handleAddButtonClick,
+    }
+  },
 })
 </script>
 
